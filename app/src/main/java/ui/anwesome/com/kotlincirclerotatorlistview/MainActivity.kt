bdbf4881d5c61ba98e6e542f53a82ea6d43e2958
Anwesh43 <anweshthecool0@gmail.com>
2018-03-03 14:27:45 +0530

    adding listener in activity

--- app/src/main/java/ui/anwesome/com/kotlincirclerotatorlistview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlincirclerotatorlistview/MainActivity.kt
@@ -2,12 +2,16 @@ package ui.anwesome.com.kotlincirclerotatorlistview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import ui.anwesome.com.circlerotatorlistview.CircleRotatorListView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        CircleRotatorListView.create(this)
+        val view = CircleRotatorListView.create(this)
+        view.addOnRotateListener {
+            Toast.makeText(this, "rotated $it", Toast.LENGTH_SHORT).show()
+        }
     }
 }
