b8fd0350048c5c0ba4ea14197f26e7504852e2e3
Anwesh43 <anweshthecool0@gmail.com>
2018-03-03 09:14:52 +0530

    adding CircleRotatorListView class

--- circlerotatorlistview/src/main/java/ui/anwesome/com/circlerotatorlistview/CircleRotatorListView.kt
+++ circlerotatorlistview/src/main/java/ui/anwesome/com/circlerotatorlistview/CircleRotatorListView.kt
@@ -3,3 +3,20 @@ package ui.anwesome.com.circlerotatorlistview
 /**
  * Created by anweshmishra on 03/03/18.
  */
+import android.view.*
+import android.content.*
+import android.graphics.*
+class CircleRotatorListView(ctx : Context) : View(ctx) {
+    val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas : Canvas) {
+
+    }
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
